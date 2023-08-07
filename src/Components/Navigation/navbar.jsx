c4e08747b2884e08b35a34c358aee79173adfc79
Daniel S <Danubia@live.de>
2023-08-07 19:32:27 +0200

    Navibar überarbeitet

--- src/Components/Navigation/navbar.jsx
+++ src/Components/Navigation/navbar.jsx
@@ -4,13 +4,20 @@ import Link from 'next/link';
 
 function Navigation() {
   return (
-    <div className={styles.navigationcontainer}> 
-        <Link href="/" className={styles.navigationlink}>Dani-Line</Link>
-        <Link href="/About" className={styles.navigationlink}>Über</Link>
-        <Link href="/Skills" className={styles.navigationlink}>Skills</Link>
-        <Link href="/Projekte" className={styles.navigationlink}>Projekte</Link>
-        <Link href="/Kontakt" className={styles.navigationlink}>Kontakt</Link>
-    </div>
+  <div className={styles.navi_wrapper}>
+
+        <div className={styles.logo}>
+             <img className={styles.logoimage} src="/assets/logo-transparent-png.png" alt="logo" />
+        </div>
+        
+        <div className={styles.navigationcontainer}> 
+            <Link href="/" className={styles.navigationlink}>Dani-Line</Link>
+            <Link href="/About" className={styles.navigationlink}>Wer wir sind</Link>
+            <Link href="/Skills" className={styles.navigationlink}>Unsere Skills</Link>
+            <Link href="/Projekte" className={styles.navigationlink}>Projekte</Link>
+            <Link href="/Kontakt" className={styles.navigationlink}>Kontakt</Link>
+        </div>
+  </div>
   )
 }
 
