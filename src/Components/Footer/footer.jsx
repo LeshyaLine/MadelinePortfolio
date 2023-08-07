65ec68d09974fdfc832b202a41a97f8577cd7d8b
Daniel S <Danubia@live.de>
2023-08-07 18:35:30 +0200

    penisnase

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from '@/Components/Footer/footer.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Footer() {
   return (
     <div className={styles.footercontainer}> 
         <Link href="/Impressum" className={styles.footerlink}>Impressum</Link>
-        {/* <Link href="/About" className={styles.footerlink}>Über</Link>
+        <p>©Madeline Streubel & Daniel Stranz</p>
+        {/* <Link href="/Facebook" className={styles.footerlink}><p><Image src="/assets/facebook.png" width={50} height={50}></Image></p></Link>
         <Link href="/Skills" className={styles.footerlink}>Skills</Link>
         <Link href="/Projekte" className={styles.footerlink}>Projekte</Link>
-        <Link href="/Kontakt" className={styles.footerlink}>Kontakt</Link> */}
-    </div>
+        <Link href="/Kontakt" className={styles.footerlink}>Kontakt</Link>*/}
+    </div> 
   )
 }
 
