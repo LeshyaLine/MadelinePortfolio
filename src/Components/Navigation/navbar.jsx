bfee611bade010554abdd9bb29df57f01073d077
Daniel S <Danubia@live.de>
2023-08-08 18:47:08 +0200

    weiter an der Navileiste grundsteine gelegt

--- src/Components/Navigation/navbar.jsx
+++ src/Components/Navigation/navbar.jsx
@@ -11,12 +11,34 @@ function Navigation() {
         </div>
         
         <div className={styles.navigationcontainer}> 
+
+        <section className={styles.rundeeckensection}>
+          <span className={styles.rundeeckenspan}>
             <Link href="/" className={styles.navigationlink}>Dani-Line</Link>
-            <Link href="/About" className={styles.navigationlink}>Wer wir sind</Link>
-            <Link href="/Skills" className={styles.navigationlink}>Unsere Skills</Link>
+          </span>
+        </section>
+        <section className={styles.rundeeckensection}>
+        <span className={styles.rundeeckenspan}>
+            <Link href="/About" className={styles.navigationlink}>Wir sind...</Link>
+            </span>
+        </section>
+        <section className={styles.rundeeckensection}>
+        <span className={styles.rundeeckenspan}>
+            <Link href="/Skills" className={styles.navigationlink}>Skills</Link>
+            </span>
+        </section>
+        <section className={styles.rundeeckensection}>
+        <span className={styles.rundeeckenspan}>
             <Link href="/Projekte" className={styles.navigationlink}>Projekte</Link>
+            </span>
+        </section>
+        <section className={styles.rundeeckensection}>
+        <span className={styles.rundeeckenspan}>
             <Link href="/Kontakt" className={styles.navigationlink}>Kontakt</Link>
+            </span>
+        </section>
         </div>
+
   </div>
   )
 }
