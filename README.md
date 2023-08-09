45528732a551cd030d96a28c57b66f256882aa92
Daniel S <Danubia@live.de>
2023-08-09 19:52:44 +0200

    MainPage vorwärts gebracht

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 
 türkisgrün dunkel: #006366
 dunkles violetrot: #9d2c6a
+font überlegung: Space grotesk
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,29 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.maincontainer}>
-        <p className={styles.pbold}>Unmögliches ist bei uns unmöglich!</p>
-        <h1 className={styles.maintitle}>Moin, wir sind <span className={styles.myname}>Dani-Line</span></h1>
-        <h1 className={styles.maintitle}>Full-Stack-Web-Agentur</h1>
+          {/* <p className={styles.pbold}>Unmögliches ist bei uns unmöglich!</p> */}
+        <div className={styles.heins}>    
+          <h1 className={styles.maintitle}>Moin, wir sind <span className={styles.myname}>Dani-Line</span></h1>
+        </div>
+
+
+        <div className={styles.hzwei}>
+          <h2 className={styles.maintitle}>Ihre <span className={styles.kreative}>kreative</span> Full-Stack-Web-Agentur</h2>
+        </div>
+
+        <div className={styles.hdrei}>
+          <h3 className={styles.maintitle}>Starten Sie mit uns digital voll durch!</h3>
+        </div>
+        <div className={styles.hdrei}>
+          <h3 className={styles.maintitle}>Starten Sie mit uns digital voll durch!</h3>
+        </div>
+        <div className={styles.hdrei}>
+          <h3 className={styles.maintitle}>Starten Sie mit uns digital voll durch!</h3>
+        </div>
+        <div className={styles.hdrei}>
+          <h3 className={styles.maintitle}>Starten Sie mit uns digital voll durch!</h3>
+        </div>
+
     </main>
   )
 }
